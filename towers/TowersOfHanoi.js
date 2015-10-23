c37dfbf848bf74cc20db49c6f5127e87940779ce
Kevin Boyle <kevin.boyle@radius.com>
2015-10-23 10:51:53 -0500

    Towers of Hanoi with gui

---
+++ towers/TowersOfHanoi.js
@@ -0,0 +1,36 @@
+
+;(function ($, root, undefined) {
+
+  'use strict';
+
+  $(document).ready(function() {
+    var block;
+
+    function checkForWin() {
+      var gameWon = false;
+      $('[data-stack]').each(function () {
+        if ($(this).data('stack') !== 1 && $(this).children().length === 4) {
+          gameWon = true;
+        }
+      });
+      return gameWon;
+    }
+
+    $('[data-stack]').on('click', function() {
+      if (!block) {
+        block = $(this).children().last().detach();
+      } else {
+        if ( !$(this).children().last().data('block') || $(this).children().last().data('block') > $(block).data('block') ) {
+          $(this).append(block);
+          block = null;
+        }
+
+        if (checkForWin()) {
+          $('#announce-game-won').text('You Won!');
+        }
+      }
+    });
+  });
+
+})(jQuery, this);
+
